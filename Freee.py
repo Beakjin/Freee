b4220bc849987d8927c7491fcdf17d288d436f53
Beakjin <ko@proszet.com>
2024-11-20 19:47:27 +0900

    1120修正

--- Freee.py
+++ Freee.py
@@ -56,6 +56,113 @@ date_label.pack(pady=5)
 # Initialize start time for break
 break_start_time = None
 
+def check_work():
+    
+
+    try:
+    
+
+        today = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+        options = Options()
+        options.add_argument("--headless")  # ヘッドレスモードでバックグラウンド実行
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--disable-gpu")
+
+        # WebDriverの起動
+        driver = webdriver.Chrome(options=options)
+        driver.get(free_url)
+        time.sleep(3)
+
+        # ログイン処理
+        login_field = driver.find_element(By.XPATH, '//*[@id="loginIdField"]')
+        login_field.send_keys(user_id)
+        password_field = driver.find_element(By.XPATH, '//*[@id="passwordField"]')
+        password_field.send_keys(user_password)
+        login_button = driver.find_element(By.XPATH, '//span[text()="ログイン"]')
+        login_button.click()
+        time.sleep(5)
+
+        # 出勤ボタンの有無を確認
+        try:
+            button = driver.find_element(By.XPATH, '//span[text()="出勤"]')
+            print("出勤ボタンが見つかりました。")
+            return  # 出勤ボタンがある場合は何もしない
+        except:
+            print("出勤ボタンが見つかりません。修正ボタンをクリックします。")
+
+        # 修正ボタンをクリック
+        fix_button = driver.find_element(By.XPATH, '//span[text()="修正"]')
+        fix_button.click()
+        time.sleep(5)
+
+        # 指定したXPathのデータを取得
+        data1, data2, data3 = None, None, None
+        try:
+            data1 = driver.find_element(By.XPATH, '(//span[@class="vb-tableListCell__text" and contains(text(), ":")])[1]').text
+            print(data1)
+        except :
+            print("データ1が見つかりませんでした。")
+            data1="1"
+            print(data1)
+    
+        try:
+            data2 = driver.find_element(By.XPATH, '(//span[@class="vb-tableListCell__text" and contains(text(), ":")])[2]').text
+            print(data2)
+        except :
+            print("データ2が見つかりませんでした。")
+            data2="1"
+            print(data2)
+        
+        try:
+            data3 = driver.find_element(By.XPATH, '(//span[@class="vb-tableListCell__text" and contains(text(), ":")])[3]').text
+            print(data3)
+        except :
+            print("データ3が見つかりませんでした。")
+            data3="1"
+            print(data3)
+        
+        
+        today1 = today.split()[1]  # startworkから時間部分のみ取得（例えば、"08:00:00"）
+        todayshu = today.split()[0] + ' ' + data1
+        
+        today1 = today.split()[1]  # startworkから時間部分のみ取得（例えば、"08:00:00"）
+        todaykyustart = today.split()[0] + ' ' + data2
+
+        today1 = today.split()[1]  # startworkから時間部分のみ取得（例えば、"08:00:00"）
+        todaykyuend = today.split()[0] + ' ' + data3
+
+        # times_labelにデータを表示
+        text_content = ""
+        if data1.strip() != '1':
+           text_content += f"出勤時刻: {todayshu}\n"
+
+        if data2.strip() != '1':
+           text_content += f"休憩開始: {todaykyustart}\n"
+
+        if data3.strip() != '1':
+           text_content += f"休憩終了: {todaykyuend}"
+
+        times_label.config(text=text_content)
+        messagebox.showinfo("Freee", "Freeeの使用準備が完了しました。")
+
+    except Exception as e:
+        print("エラーが発生しました:", e)
+
+    finally:
+        # WebDriverを閉じる
+        try:
+            driver.quit()
+        except:
+            pass
+
+messagebox.showinfo("Freee", "OKを押すとステータス確認を行います。しばらくお待ちください。")
+
+# アプリ起動後すぐに処理を実行
+window.after(100, check_work)
+
+
 # Functions for each action button
 def on_ok():
     # 現在の出勤時刻を取得
@@ -121,7 +228,7 @@ def on_start():
     
     times_label.config(text=times_label.cget("text") + f"休憩開始: {start}\n")
     window.after(1000, on_Cliqkyuukei_start)
-    window.after(3540000, on_kyuukei_end)
+    window.after(3600000, on_kyuukei_end)
 
     options = Options()
     options.add_argument("--headless")  # ヘッドレスモードでバックグラウンド実行
@@ -147,10 +254,11 @@ def on_start():
 
 
 def on_end():
+    messagebox.showinfo("Freee", "今日も一日お疲れ様でした。")
     global taikin
     taikin = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-    window.after(1000, on_Cliqwork_start)
+    window.after(1000, on_Cliqwork_end)
 
     options = Options()
     options.add_argument("--headless")  # ヘッドレスモードでバックグラウンド実行
@@ -173,8 +281,9 @@ def on_end():
     button1 = driver.find_element(By.XPATH, '//span[text()="退勤"]')
     button1.click()
     time.sleep(5)
-    window.quit()
+    
 
+    
 def on_Cliqkyuukei_start():
 
     options = Options()
@@ -197,12 +306,38 @@ def on_Cliqkyuukei_start():
     time.sleep(5)
     login_button = driver.find_element(By.XPATH, '//*[@id="nextbtn"]/span')
     login_button.click()
+    time.sleep(10)
+    try:
+        # 要素を探してクリック
+        button = driver.find_element(By.XPATH, '//a[@class="succbutton" and text()="確認する"]')
+        button.click()
+        print("ボタンをクリックしました。")
+    except:
+        # 要素が見つからない場合はスキップ
+        print("ボタンが見つかりませんでした。処理をスキップします。")
     time.sleep(5)
+    max_retries = 5  # 最大試行回数
+    attempts = 0
+    while attempts < max_retries:
+        try:
+             continue_button = driver.find_element(By.XPATH, '//a[text()="同意する"]')
+             continue_button.click()
+             print("次のボタンがクリックされました。")
+             break
+        except:
+             attempts += 1
+             print("次のボタンが見つかりませんでした。")
+             if attempts < max_retries:
+                time.sleep(2)  # 2秒待機してから再試行
+             else:
+                 print("最大試行回数に達しました。処理を終了します。")
+
+    time.sleep(8)
     status_text = driver.find_element(By.XPATH, '//span[@class="meal-brk mR6"]')
     status_text.click()
     time.sleep(1)
 
-def on_Cliqwork_start():
+def on_Cliqkyuukei_end():
 
     options = Options()
     options.add_argument("--headless")  # ヘッドレスモードでバックグラウンド実行
@@ -224,15 +359,151 @@ def on_Cliqwork_start():
     time.sleep(5)
     login_button = driver.find_element(By.XPATH, '//*[@id="nextbtn"]/span')
     login_button.click()
+    time.sleep(10)
+    try:
+        # 要素を探してクリック
+        button = driver.find_element(By.XPATH, '//a[@class="succbutton" and text()="確認する"]')
+        button.click()
+        print("ボタンをクリックしました。")
+    except:
+        # 要素が見つからない場合はスキップ
+        print("ボタンが見つかりませんでした。処理をスキップします。")
+    time.sleep(5)
+    max_retries = 5  # 最大試行回数
+    attempts = 0
+    while attempts < max_retries:
+        try:
+             continue_button = driver.find_element(By.XPATH, '//a[text()="同意する"]')
+             continue_button.click()
+             print("次のボタンがクリックされました。")
+             break
+        except:
+             attempts += 1
+             print("次のボタンが見つかりませんでした。")
+             if attempts < max_retries:
+                time.sleep(2)  # 2秒待機してから再試行
+             else:
+                 print("最大試行回数に達しました。処理を終了します。")
+    time.sleep(8)
+    status_text = driver.find_element(By.XPATH, "//div[contains(@class, 'laptop-work')]")
+    status_text.click()
+    time.sleep(1)
+
+def on_Cliqwork_start():
+
+    options = Options()
+    options.add_argument("--headless")  # ヘッドレスモードでバックグラウンド実行
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    
+    driver = webdriver.Chrome(options=options)
+    driver.get(Cliq_url)
+    time.sleep(3)
+    
+    login_field = driver.find_element(By.XPATH, '//*[@id="login_id"]')
+    login_field.send_keys(user_id)
+    next_button = driver.find_element(By.XPATH, '//span[text()="次へ"]')
+    next_button.click()    
+    time.sleep(5)
+    password_field = driver.find_element(By.XPATH, '//*[@id="password"]')
+    password_field.send_keys(user_password)
+    time.sleep(5)
+    login_button = driver.find_element(By.XPATH, '//*[@id="nextbtn"]/span')
+    login_button.click()
+    time.sleep(10)
+    try:
+        # 要素を探してクリック
+        button = driver.find_element(By.XPATH, '//a[@class="succbutton" and text()="確認する"]')
+        button.click()
+        print("ボタンをクリックしました。")
+    except:
+        # 要素が見つからない場合はスキップ
+        print("ボタンが見つかりませんでした。処理をスキップします。")
+
+    max_retries = 5  # 最大試行回数
+    attempts = 0
+    while attempts < max_retries:
+        try:
+             continue_button = driver.find_element(By.XPATH, '//a[text()="同意する"]')
+             continue_button.click()
+             print("次のボタンがクリックされました。")
+             break
+        except:
+             attempts += 1
+             print("次のボタンが見つかりませんでした。")
+             if attempts < max_retries:
+                time.sleep(2)  # 2秒待機してから再試行
+             else:
+                 print("最大試行回数に達しました。処理を終了します。")
+    
+    time.sleep(8)
+    status_text = driver.find_element(By.XPATH, '//span[@class="slider"]')
+    status_text.click()
+    time.sleep(1)
+
+def on_Cliqwork_end():
+
+    options = Options()
+    options.add_argument("--headless")  # ヘッドレスモードでバックグラウンド実行
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    
+    driver = webdriver.Chrome(options=options)
+    driver.get(Cliq_url)
+    time.sleep(3)
+    
+    login_field = driver.find_element(By.XPATH, '//*[@id="login_id"]')
+    login_field.send_keys(user_id)
+    next_button = driver.find_element(By.XPATH, '//span[text()="次へ"]')
+    next_button.click()    
+    time.sleep(5)
+    password_field = driver.find_element(By.XPATH, '//*[@id="password"]')
+    password_field.send_keys(user_password)
+    time.sleep(5)
+    login_button = driver.find_element(By.XPATH, '//*[@id="nextbtn"]/span')
+    login_button.click()
+    
+    time.sleep(5)
+    try:
+        # 要素を探してクリック
+        button = driver.find_element(By.XPATH, '//a[@class="succbutton" and text()="確認する"]')
+        button.click()
+        print("ボタンをクリックしました。")
+    except:
+        # 要素が見つからない場合はスキップ
+        print("ボタンが見つかりませんでした。処理をスキップします。")
+
+    time.sleep(5)
+    max_retries = 5  # 最大試行回数
+    attempts = 0
+    while attempts < max_retries:
+        try:
+             continue_button = driver.find_element(By.XPATH, '//a[text()="同意する"]')
+             continue_button.click()
+             print("次のボタンがクリックされました。")
+             break
+        except:
+             attempts += 1
+             print("次のボタンが見つかりませんでした。")
+             if attempts < max_retries:
+                time.sleep(2)  # 2秒待機してから再試行
+             else:
+                 print("最大試行回数に達しました。処理を終了します。")
+    time.sleep(5)
+
     time.sleep(5)
     status_text = driver.find_element(By.XPATH, '//span[@class="slider"]')
     status_text.click()
     time.sleep(1)
+    window.quit()
 
 def on_kyuukei_end():
     global end
     end = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     times_label.config(text=times_label.cget("text") + f"休憩終了: {end}\n")
+    #window.after(1000, on_Cliqkyuukei_end)
 
 
     options = Options()
@@ -292,21 +563,20 @@ def show_description_window():
                               , wraplength=350, justify="left")
     description_label1.pack(pady=2)
 
-    description_label2 = tk.Label(description_window, text="注意 : 出勤時について\n", wraplength=350, justify="left", fg="red")
+    description_label2 = tk.Label(description_window, text="Cliq連携", wraplength=350, justify="left", fg="red")
     description_label2.pack(pady=2)
 
-    description_label3 = tk.Label(description_window, text="出勤時に、前回の出勤実績が残っている（退勤を押せていない）場合は\n"
-                                    "Freeeの仕様上退勤ボタンのみが残り、出勤が押せないため\n"
-                                    "手動で退勤処理を行った後、ツール上で出勤ボタンを押下してください。\n"
+    description_label3 = tk.Label(description_window, text="CliqとFreeeのアカウント情報を同一しておくと\n"
+                                    "出勤・休憩開始・退勤の際に連携してステータスを変更します。\n"
+
                             , wraplength=350, justify="left")
     description_label3.pack(pady=2)
 
-    description_label4 = tk.Label(description_window, text="休憩時間について\n", wraplength=350, justify="left", fg="red")
+    description_label4 = tk.Label(description_window, text="休憩について\n", wraplength=350, justify="left", fg="red")
     description_label4.pack(pady=2)
 
-    description_label5 = tk.Label(description_window, text="出勤後5時間たっても、休憩開始をしていない場合休憩実施確認が表示されます。\n"
-                                    "休憩をとっていないと毎月の給与精算処理が正常に行えないため必ず休憩処理をお願いします。\n"
-                                    "（休憩確認表示時すでに実際に休憩をとっている場合は、～～を押してください。自動で休憩処理を行います。）\n"
+    description_label5 = tk.Label(description_window, text="休憩開始60分後に自動で休憩終了を打刻します。\n"
+
                                     , wraplength=350, justify="left")
     description_label5.pack(pady=2)
 
@@ -315,20 +585,22 @@ times_label = tk.Label(window, text="", justify="left")
 times_label.pack(pady=10)
 
 # Create buttons
-ok_button = tk.Button(window, text="出勤", command=on_ok)
-ok_button.pack(side="left", padx=10, pady=10)
+ok_button = tk.Button(window, text="出勤", command=on_ok, width=5, height=1)
+ok_button.pack(side="left", padx=15, pady=15)
 
-start_button = tk.Button(window, text="休憩開始", command=on_start)
-start_button.pack(side="left", padx=10, pady=10)
+start_button = tk.Button(window, text="休憩開始", command=on_start, width=7, height=1)
+start_button.pack(side="left", padx=15, pady=15)
 
-home_button = tk.Button(window, text="ホーム", command=on_home)
-home_button.pack(side="left", padx=10, pady=10)
-
-end_button = tk.Button(window, text="退勤", command=on_end)
+end_button = tk.Button(window, text="退勤", command=on_end, width=5, height=1)
 end_button.pack(side="right", padx=10, pady=10)
 
-kyuukei_end_button = tk.Button(window, text="休憩終了", command=on_kyuukei_end)
-kyuukei_end_button.pack(side="right", padx=10, pady=10)
+home_button = tk.Button(window, text="ホーム", command=on_home, width=5, height=1)
+home_button.pack(side="right", padx=30, pady=30)
+
+
+
+#kyuukei_end_button = tk.Button(window, text="休憩終了", command=on_kyuukei_end, width=5, height=1)
+#kyuukei_end_button.pack(side="right", padx=10, pady=10)
 
 description_button = tk.Button(window, text="説明を見る", command=show_description_window)
 description_button.place(x=310, y=10, anchor="ne")
